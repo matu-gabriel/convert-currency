9626e2260fe38ad4d7562cdc9fa75cbda76f33e6
matu-gabriel <matu.dev25@gmail.com>
2023-11-20 18:22:28 -0300

    Att de funcoes

--- Convert-Money/js/scripts.js
+++ Convert-Money/js/scripts.js
@@ -2,24 +2,50 @@ const buttonConvert = document.querySelector(".button-convert");
 const currentValueToConvert = document.querySelector(
   ".current-value-to-convert"
 );
+const selectOption = document.querySelector(".select-option");
 const currentValeu = document.querySelector(".current-value");
 
 function convertMoney() {
   const inputCurrency = document.querySelector(".input-currency").value;
 
-  const dolarToday = 5.1;
-
-  const convertValue = inputCurrency / dolarToday;
+  if (selectOption.value == "dolar") {
+    currentValeu.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrency / 5.1);
+  } else if (selectOption.value == "euro") {
+    currentValeu.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrency / 5.31);
+    // } else if (selectOption.value == "libra") {
+    //   currentValeu.innerHTML = new Intl.NumberFormat("", {
+    //     style: "currency",
+    //     currency: "",
+    //   });
+  }
 
   currentValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   }).format(inputCurrency);
-
-  currentValeu.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(convertValue);
 }
+
+function changeCurrency() {
+  const currencyName = document.querySelector("#currency-name");
+  const countryFlag = document.querySelector(".country-flag");
+
+  if (selectOption.value == "dolar") {
+    currencyName.innerHTML = "Dólar";
+    countryFlag.src = "./assets/estados-unidos.png";
+  } else if (selectOption.value == "euro") {
+    currencyName.innerHTML = "Euro";
+    countryFlag.src = "./assets/euro.png";
+  }
+
+  convertMoney();
+}
+
+selectOption.addEventListener("change", changeCurrency);
 
 buttonConvert.addEventListener("click", convertMoney);
